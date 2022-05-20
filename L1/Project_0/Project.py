e0c599e5d75fe99ad5aa4abad490c8bc54fcdb3f
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-20 23:52:54 +0300

    Initial commit

---
+++ L1/Project_0/Project.py
@@ -0,0 +1,20 @@
+a = 12
+b = 21
+c = a + b
+print(c)
+
+s = "hello \n'world"
+print(s)  # вывод строки
+print('{} - {} - {} - {}'.format(a, b, c, s))
+print(f'{a} - {b} - {c} - {s}')
+
+f = True
+
+num_list = [1, 2, 4]
+print(num_list)
+
+g = int(input())
+p = int(input())
+
+result = round(g * p, 5)  # колличество знаков после запятой
+print(result)
