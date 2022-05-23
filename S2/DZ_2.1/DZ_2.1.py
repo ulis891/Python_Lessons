3a4f6c15819832a612287fb3850e9f607dd2f2a4
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-24 00:00:36 +0300

    add DZ_2.1

---
+++ S2/DZ_2.1/DZ_2.1.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+result_list = []
+
+while True:
+    try:
+        number = int(input("Введите число: "))
+        for i in range(1, number + 1):
+            if i == 1:
+                result_list.append(i)
+            else:
+                result_list.append(result_list[i - 2] * i)
+        break
+    except ValueError:
+        print("Вы ввели неверные данные")
+
+print(result_list)
