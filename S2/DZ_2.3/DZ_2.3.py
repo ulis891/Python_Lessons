7b93de4f7f4f0272fe6486fc4674bfe121657d9a
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-26 23:47:30 +0300

    add DZ_2.3

---
+++ S2/DZ_2.3/DZ_2.3.py
@@ -0,0 +1,20 @@
+'''Реализуйте алгоритм перемешивания списка'''
+import random
+
+list = ['мама', 'мыла', 'раму']
+
+
+# random.shuffle(list)
+# print(list)
+
+
+def list_shuffle(list_to_shuffle):
+    new_list = list_to_shuffle
+    for i in range(len(new_list) - 1, 0, -1):
+        j = random.randint(0, i)
+        new_list[i], new_list[j] = new_list[j], new_list[i]
+    return new_list
+
+
+print(f'Начальный список: {list} \nИзмененный список: {list_shuffle(list)}')
+
