8feb478a0f278b1fa62dd48671bfaa5a8bfcb098
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-29 14:58:19 +0300

    add L2

---
+++ L2/Project_0/main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'a') # 'a' - данные дописываются, 'w' - данные перезаписываются
+# #data.writelines(colors) # Разделителей не будет
+# data.write('\nLINE 2\n')
+# data.write('LINE 3\n')
+# data.close()
+
+
+
+path = 'file.txt'
+data = open(path, 'r')
+for line in data:
+    print(line)
+data.close()
