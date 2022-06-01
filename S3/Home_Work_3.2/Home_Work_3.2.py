37e0713c3bdbb8b4ae25480a15ac306548ca6e3c
Aleksandr Kostromin <ulis89@yandex.ru>
2022-06-01 15:31:06 +0300

    UPDHome_Work_3.2

--- S3/Home_Work_3.2/Home_Work_3.2.py
+++ S3/Home_Work_3.2/Home_Work_3.2.py
@@ -5,12 +5,12 @@ numbers_list = [12, 4, 55, 2, 66, 86, 4, 80, 5, 2, 5, 3, 4]
 
 
 def multiplication_pairs(list):
-    sum_list = []
+    multi_list = []
     for i in range(0, len(list) // 2):
-        sum_list.append(list[i] * list[len(list) - (1 + i)])
-    if len(list) > (len(sum_list) * 2):
-        sum_list.append((list[len(list) // 2]) ** 2)
-    return sum_list
+        multi_list.append(list[i] * list[len(list) - (1 + i)])
+    if len(list) > (len(multi_list) * 2):
+        multi_list.append((list[len(list) // 2]) ** 2)
+    return multi_list
 
 
 print(multiplication_pairs(numbers_list))
