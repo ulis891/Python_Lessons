2fa5c3830811be7ff9614b7a9bfe7f6ca54645fb
alrksandrkostromin <ulis89@yandex.ru>
2023-06-11 20:47:00 +0300

    UPD HW7.2

--- S7.2/HW7.2.py
+++ S7.2/HW7.2.py
@@ -17,8 +17,7 @@ slog_dic = 'йуеыаоэяию'
 count_dic = {}
 count_list = []
 
-poem = 'пара-ра-рам рам-пам-папам па-ра-па-да'
-# poem = input("Введите стих: ")
+poem = input("Введите стих: ")
 
 s1 = poem.split(' ')
 
@@ -28,14 +27,13 @@ for i in s1:
         if char in slog_dic:
             count_dic[i] += 1
 
-for j,p in count_dic.items():
+for j, p in count_dic.items():
     count_list.append(p)
-    
-if len(list(filter(lambda x: x-count_list[0] == 0, count_list ))) != len(count_list):
+
+if len(list(filter(lambda x: x - count_list[0] == 0, count_list))) != len(count_list):
     print("Пам парам")
 else:
     print("Парам пам-пам")
-
 
 """
 Задача 36:
@@ -54,31 +52,32 @@ else:
 **Вывод:**
 """
 
-def create_matrix (func, a, b):
-    
-    arr = [i for i in range(1,a+1)]
-    for l in arr:
-        print(l, end=' ')
-    print('\n')
+
+def print_operation_table(func, a: int, b: int):
+    """Печатает таблицу """
+    arr = [i for i in range(1, a + 1)]
     for k in arr:
-        # print(k, end='   ')
-        for j in range(1, b):
-            res = func(k,j)
-            print(res, end=' ')
+        print_char(k)
+    print('\n')
+    for j in range(2, b + 1):
+        print_char(j)
+        for k in arr[1:]:
+            print_char(func(k, j))
+
         print('\n')
-        # res = list(map(func, arr))
-        # for j in res:
-        #     if len(str(j)) == 1:
-        #         print(j, end=' ' * 3)
-        #     elif len(str(j)) == 2:
-        #         print(j, end=' ' * 2)
-        #     else:
-        #         print(j, end=' ')
-        # print('\n')
 
-# print_operation_table(lambda x, y: x * y)
 
-            # lambda t: t + k
+def print_char(char_in_table: int):
+    """Печатает число с правильным отступом"""
+    if len(str(char_in_table)) == 1:
+        print(char_in_table, ' ' * 3, end='')
+    elif len(str(char_in_table)) == 2:
+        print(char_in_table, ' ' * 2, end='')
+    else:
+        print(char_in_table, ' ', end='')
 
-# print_matrix(array1)
-create_matrix((lambda t,k: t + k), 5 , 5)
+
+num_columns = int(input('Введите количество колонок: '))
+num_rows = int(input('Введите количество строк: '))
+
+print_operation_table((lambda c, r: c + r), num_columns, num_rows)
