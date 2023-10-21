0bc479b2124cc1cd4c2f383e18fc1263a34499af
Aleksandr Kostromin <ulis89@yandex.ru>
2023-10-21 20:16:41 +0300

    upd S8 HW

--- immersion in python/S8/HW/HW8.py
+++ immersion in python/S8/HW/HW8.py
@@ -1,7 +1,7 @@
 import os
-from mod_for_files import file as mof
+from mod_for_files import file as mff
 
 
 if __name__ == '__main__':
     parent_dir = os.path.dirname(os.path.dirname(os.getcwd()))
-    mof.traverse_directory(parent_dir)
+    mff.traverse_directory(parent_dir)
